9090100f403ab730012ba4ebf52058ea3bf1f826
Juan-Pablo Barahona <juanpablo.barahonaa@gmail.com>
2024-10-02 11:20:59 -0500

    0.6 env FUNCTIONAPI

--- src/services/execute-flow/execute-flow.modules.js
+++ src/services/execute-flow/execute-flow.modules.js
@@ -1,5 +1,6 @@
 const hydrate = require('feathers-sequelize/hooks/hydrate');
 const axios = require('axios');
+const { FUNCTIONAPI } = process.env;
 
 module.exports = {
 	// get employee associated to the user request
@@ -43,19 +44,17 @@ module.exports = {
 					rutaArchivo: ''
 				});
 
-				// // ejecutar functions
-				// axios.post(`http://localhost:7071/api/${proceso}`, {
-				// 	executeFlowId: id,
-				// 	periodo: data.periodo,
-				// 	ceco: data.ceco,
-				// 	nuevaRevision: data.nuevaRevision
-				// })
+				// ejecutar functions
+				axios.post(`${FUNCTIONAPI}/api/${proceso}`, {
+					executeFlowId: id,
+					periodo: data.periodo,
+					ceco: data.ceco,
+					nuevaRevision: data.nuevaRevision
+				})
 			});
 
-			if(!data.procesos.includes('prebuk') && !data.procesos.includes('buk')){
-				axios.post('https://prod-06.eastus.logic.azure.com:443/workflows/e47ae2c4ac524232af167cbea7e53d1b/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=RSsY7D8OCLC9cSQCAae0IVpeoQNkyhzFG8M1Msx95NQ', data);
-			}else if(data.procesos.includes('buk')){
-				axios.post(`ei-nexxo-matrices-estados-cuentas-microflujos.azurewebsites.net/api/prebuk`, {
+			if(data.procesos.includes('buk')){
+				axios.post(`${FUNCTIONAPI}/api/prebuk`, {
 					executeFlowId: id,
 					periodo: data.periodo,
 					ceco: data.ceco,
@@ -63,7 +62,7 @@ module.exports = {
 					buk: true
 				})
 			} else {
-				axios.post(`ei-nexxo-matrices-estados-cuentas-microflujos.azurewebsites.net/api/prebuk`, {
+				axios.post(`${FUNCTIONAPI}/api/prebuk`, {
 					executeFlowId: id,
 					periodo: data.periodo,
 					ceco: data.ceco,
